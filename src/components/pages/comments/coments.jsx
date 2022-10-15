c872da16a555a880d267ff9822d4c830a59a2b01
simlenodir <nodirsmailov0@gmail.com>
2022-10-15 23:20:33 +0300

    sss

--- src/components/pages/comments/coments.jsx
+++ src/components/pages/comments/coments.jsx
@@ -1,9 +1,4 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
 export const Comments = () => {
-  const params = useParams();
-  const [comments, setComments] = useState();
-
   return (
     <div className="container">
       <h2>Comments page</h2>
